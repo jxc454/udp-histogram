701467a7383d742073265faf0ccd4b47799314bf
Jameson Cotter <jameson.cotter@gmail.com>
2019-06-15 19:10:43 -0400

    updating PbIntMapDeserializer.

--- src/main/scala/com/jxc454/udpstreaminganalytic/serializers/PbIntMapDeserializer.scala
+++ src/main/scala/com/jxc454/udpstreaminganalytic/serializers/PbIntMapDeserializer.scala
@@ -1,5 +1,14 @@
 package com.jxc454.udpstreaminganalytic.serializers
 
-class PbIntMapDeserializer {
+import java.util
 
+import com.jxc454.models.SimpleMessages.SimpleIntMap
+import org.apache.kafka.common.serialization.Deserializer
+
+class PbIntMapDeserializer extends Deserializer[SimpleIntMap] {
+  override def deserialize(topic: String, bytes: Array[Byte]): SimpleIntMap = SimpleIntMap.parseFrom(bytes)
+
+  override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = ()
+
+  override def close(): Unit = ()
 }
