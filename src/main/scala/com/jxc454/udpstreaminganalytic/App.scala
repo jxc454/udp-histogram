57e9b932bde5a12faacab216b17e748050e0c9d1
unknown <jameson.cotter@gmail.com>
2019-06-14 20:24:43 -0400

    streams placeholder

--- src/main/scala/com/jxc454/udpstreaminganalytic/App.scala
+++ src/main/scala/com/jxc454/udpstreaminganalytic/App.scala
@@ -1,12 +1,19 @@
 package com.jxc454.udpstreaminganalytic
 
 import com.jxc454.models.SimpleMessages.SimpleIntMap
-
+import org.apache.kafka.common.serialization.{Serde, Serdes, StringDeserializer}
+import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
+import org.apache.kafka.streams.scala.StreamsBuilder
+import org.apache.kafka.streams.scala.kstream.KStream
 import scala.collection.JavaConverters._
 
 object App {
 
-  def main(args : Array[String]): Unit = ConsumerCreator.run(intToProtobuf, Processor.process, new JsonProducerCreator)
+  def main(args : Array[String]): Unit = {
+//    ConsumerCreator.run(intToProtobuf, Processor.process, new JsonProducerCreator)
+
+
+  }
 
   def intToProtobuf(frequencies: Map[Int, Int]): SimpleIntMap =
     SimpleIntMap.newBuilder().putAllFrequencies(
